365738e7e003602cd0f947ed4b9199509de11daf
Xalid <siyovushartigaliyev12@gmail.com>
2025-05-30 00:44:22 +0500

    remove cloud_backend directory

--- cloud_backend/app/core/config.py
+++
@@ -1,1 +0,0 @@
-DATABASE_URL = "postgresql://xalid:19871205@localhost:5433/oshxona"
--- cloud_backend/app/schemas/serve.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class ServeMealRequest(BaseModel):
-    meal_id: int
-
-class MealPortionOut(BaseModel):
-    meal_name: str
-    portions: int
